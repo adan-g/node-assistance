1fbda91b000296b04b3f42095d30fc8329e7dcd4
Adan Gimenez <adangimenez@Adans-MBP.attlocal.net>
2022-03-24 20:24:28 -0500

    result by weeks

--- router.js
+++ router.js
@@ -4,14 +4,15 @@ const query = require('./database/db')
 const { DateTime } = require("luxon");
 const crud = (require('./controllers/crud'));
 
-router.get('/', async(req, res) => {
-    const week = DateTime.now().weekNumber
+router.get('/:week', async(req, res) => {
+    const week = req.params.week;
   
     try{   
+      //const week = await query('SELECT DISTINCT(week) FROM assistance ORDER BY week ASC');
       const results = await query('SELECT * FROM assistance WHERE week = ?', week);
 
       const totalHoursWeek = await query('SELECT SEC_TO_TIME(SUM(TIME_TO_SEC(TIMEDIFF(a.exit, a.entry))) - SUM(TIME_TO_SEC(a.lunch))) AS hours from assistance a WHERE a.week = ?', week);
-      
+
       res.render('index', {
         results,
         totalHoursWeek
@@ -20,6 +21,26 @@ router.get('/', async(req, res) => {
     }catch(error){
       console.log(error)
     }  
+});
+
+
+router.get('/', async(req, res) => {
+  const week = DateTime.now().weekNumber
+
+  try{   
+    //const week = await query('SELECT DISTINCT(week) FROM assistance ORDER BY week ASC');
+    const results = await query('SELECT * FROM assistance WHERE week = ?', week);
+
+    const totalHoursWeek = await query('SELECT SEC_TO_TIME(SUM(TIME_TO_SEC(TIMEDIFF(a.exit, a.entry))) - SUM(TIME_TO_SEC(a.lunch))) AS hours from assistance a WHERE a.week = ?', week);
+
+    res.render('index', {
+      results,
+      totalHoursWeek
+    });
+    
+  }catch(error){
+    console.log(error)
+  }  
 });
 
 
